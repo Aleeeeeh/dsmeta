5c228eea5ce439a1bb955b47f1a188f9a8623bee
Aleeeeeh <alefesilva097@gmail.com>
2022-09-10 19:26:18 -0300

    Date select

--- dsmeta/backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ dsmeta/backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,10 +1,11 @@
 package com.devsuperior.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -19,8 +20,16 @@ public class SaleController {
 	@Autowired
 	private SaleService service;
 	
+	///Sintaxe da requisição http://localhost:8080/sales?minDate=2022-01-01&maxDate=2022-01-31
+	
 	@GetMapping
-	public List<Sale> findSales(){
-		return service.findSales();
+	public Page<Sale> findSales( 
+			
+			// requestParam value é o que recebemos, e caso não venha nada define vazio 
+			@RequestParam(value = "minDate", defaultValue = "") String minDate, 
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+			Pageable pageable // Pageable ele retorna um padrão de 20 registros
+								){ 
+		return service.findSales(minDate, maxDate, pageable);
 	}
 }
